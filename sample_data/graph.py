468b4c3c54646c244c702ee28ac257b6a928f91e
domipm <dpas602@gmail.com>
2024-11-10 14:00:41 +0100

    Updated graph script, new graphs

--- sample_data/graph.py
+++ sample_data/graph.py
@@ -1,46 +1,61 @@
 import json
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-fdir = "./data/"    # Directory where data file stored
-fname = "2050.json" # Name of the file to plot
+# Directory where data file stored
+fdir = "./data/"
+# Directory to save plots
+gdir = "./graphs/"
 
-file = open(fdir + fname)
-data = json.load(file)
+# List of all files available
+fnames = []
 
-# Plot relevant properties
-properties = ["Temperature Humidity", "Humidity", "Temperature Wash Tank"]
-for property in properties:
+for file in os.listdir(fdir):
+    fnames.append(file)
 
-    prop = []
-    tick = []
+# Sort filenames list
+fnames = np.sort(fnames)
+
+for fname in fnames:
+
+    plt.close()
+
+    f = open(fdir + fname)
+    data = json.load(f)
+
+    properties = ["Temperature Humidity", "Humidity", "Temperature Wash Tank"]
+
+    for property in properties:
+
+        prop = []
+        tick = []
+
+        for line in data['Logs']:
+            if line['name'] == property: # and line['tick'] > 834219:
+                prop.append(line['value'])
+                tick.append(line['tick']/1000/60)
+
+        plt.plot(np.asarray(tick, float), np.asarray(prop, float), linestyle="-", linewidth=1, marker=".", label=str(property))
+
+    # Plot relevant phases
+    tick_phase = []
+    phase_type = []
 
     for line in data['Logs']:
-        if line['name'] == property: # and line['tick'] > 834219:
-            prop.append(line['value'])
-            tick.append(line['tick']/1000/60)
+        if line['name'] == "Phase type":
+            tick_phase.append(line['tick']/1000/60)
+            phase_type.append(line['value'])
 
-    plt.plot(np.asarray(tick, float), np.asarray(prop, float), linestyle="-", linewidth=1, marker=".", label=str(property))
+    plt.vlines(tick_phase, -10, 120, colors="black", linestyle="--")
+    for i in range(len(phase_type)):
+        plt.text(x=tick_phase[i]+0.05, y=-10+5*i, s=phase_type[i])
 
-# Plot relevant phases
-tick_phase = []
-phase_type = []
+    # Show plot
+    plt.title("Properties over Time")
+    plt.xlabel(r"Time $t$ [min]")
+    plt.ylabel("Value")
 
-for line in data['Logs']:
-    if line['name'] == "Phase type":
-        tick_phase.append(line['tick']/1000/60)
-        phase_type.append(line['value'])
+    plt.legend()
 
-plt.vlines(tick_phase, -10, 120, colors="black", linestyle="--")
-for i in range(len(phase_type)):
-    plt.text(x=tick_phase[i]+0.05, y=-10+5*i, s=phase_type[i])
-
-# Show plot
-plt.title("Properties over Time")
-plt.xlabel(r"Time $t$ [min]")
-plt.ylabel("Value")
-
-plt.legend()
-
-plt.savefig(str( "./graphs/" + fname.split(".")[0] + ".png" ), dpi=300)
-plt.show()
+    plt.savefig(str( "./graphs/" + fname.split(".")[0] + ".png" ), dpi=300)
